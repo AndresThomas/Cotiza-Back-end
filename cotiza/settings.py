94f9537376c895e35ae334385f651f7a020c95f7
andres alejandro garcia thomas <173218@ids.upchiapas.edu.mx>
2021-03-01 15:15:28 -0600

    add heroku

--- cotiza/settings.py
+++ cotiza/settings.py
@@ -144,5 +144,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "http://127.0.0.1:4200",
-    #"https://cotizaapp2.herokuapp.com/",
+    "https://cotizaapp2.herokuapp.com/",
 ]
